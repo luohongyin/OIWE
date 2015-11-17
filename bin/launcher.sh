ae6300754801cc1b2bc9195bf305670e78774a72
SkTim <lhythu@gmail.com>
2015-11-17 15:55:22 +0800

    Update launcher.sh

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,4 +1,4 @@
 #lhy
 #2015.4
 
-./word2vec -train ../../nnse/text8 -output text8.txt -size 400 -threads 24 -binary 0 -cbow 0 -negative 1
+./word2vec -train ../../nnse/text8 -output text8.txt -size 400 -threads 24 -binary 0 -cbow 0 -negative 1 -iter 15
